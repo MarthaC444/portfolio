ba16946ee90754905540bef9c813f4a54d4577f8
MarthaC444 <MarthaChamberlain444@gmail.com>
2022-10-12 16:24:48 -0400

    Create basic footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,37 @@
-import React from "react";
+import Image from "react-bootstrap/Image";
+import Logo from "../../assets/monogram-blk-30.png";
+import "./styles.css";
 
 function Footer(props) {
   return (
-  <div>
-  <h1>Footer here</h1>
-  </div>)
+    <div className="container">
+      <div className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
+          <Image className="m-2" alt="" src={Logo}></Image>
+          <span className="text-muted">&copy; 2022 Martha Chamberlain</span>
+        </div>
+
+        <ul className="nav col justify-content-end d-flex">
+          <li className="m-1 social-space">
+            <a href="https://github.com/MarthaC444">
+              <i
+                className="bi bi-github text-muted"
+                style={{ fontSize: 30 }}
+              ></i>
+            </a>
+          </li>
+          <li className="m-1 social-space">
+            <a href="https://linkedin.com/in/mchamberlain444">
+              <i
+                className="bi bi-linkedin text-muted"
+                style={{ fontSize: 30 }}
+              ></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
