d4c3c7e27b97249b9cb37c23e19a23a196b8d2bf
MarthaC444 <MarthaChamberlain444@gmail.com>
2022-10-14 16:09:26 -0400

    Convert bootstrap to react-bootstrap components

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,26 +1,34 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import Headshot from "../../assets/headshot.jpg";
 import "./styles.css";
 
 function Services(props) {
   return (
-    <section className="container my-5 mx-auto">
-      <h2 className="text-uppercase fw-bold text-center pb-3">What I do</h2>
-      <div className="container row d-flex py-3 align-items-center">
-        <div className="container d-flex justify-content-center col-sm-6">
-          <Image
-            src={Headshot}
-            className="rounded-circle shadow"
-            width="200"
-          ></Image>
-        </div>
-        <p className="col-sm-6 py-3 change-alignment">
-          I've focused on
-          React.js but would love the opportunity to expand my experience into
-          other frameworks. If you located in the Tri-City area of east Tennessee or have a remote position, I'd love to hear from you.
-        </p>
-      </div>
+    <section>
+      <Container className="my-5 mx-auto">
+        <h2 className="pb-3 text-uppercase fw-bold text-center">What I do</h2>
+        <Row className="d-flex py-3 align-items-center">
+          <Col sm={6} className="d-flex justify-content-center">
+            <Image
+              src={Headshot}
+              className="rounded-circle shadow"
+              width="200"
+            ></Image>
+          </Col>
+          <Col sm={6}>
+            <p className="pe-4 change-alignment">
+              I've focused on React.js but would love the opportunity to expand
+              my experience into other frameworks. If you located in the
+              Tri-City area of east Tennessee or have a remote position, I'd
+              love to hear from you.
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 }
