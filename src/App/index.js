32f4d53217194fc37ce8418854dd6f9c320638f4
MarthaC444 <MarthaChamberlain444@gmail.com>
2022-10-21 14:10:39 -0400

    Move Home from component to views

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Home from "../components/Home";
+import Home from "../views/Home";
 import "./styles.css";
 
 function App() {
