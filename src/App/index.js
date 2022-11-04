270f06d199c8e28bcf3824bdaaece03c9a664bbb
MarthaC444 <MarthaChamberlain444@gmail.com>
2022-11-04 06:34:59 -0400

    Add react browser router and react hash router

--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,51 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, createRoutesFromElements, createBrowserRouter, RouterProvider } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Home from "../views/Home";
 import Resume from "../views/Resume";
 import "./styles.css";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
+// import Home, { homeLoader } from "../views/Home";
+// import Resume, { resumeLoader } from "../views/Resume";
+
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
         <Route path="/" element={<Home />} />
         <Route path="/resume" element={<Resume />} />
-      </Routes>
-    </Router>
+      </>
+      )
+    );
+    
+    // const router = createBrowserRouter([
+    //   {
+    //     path: "/",
+    //     element: <Home />,
+    //     loader: homeLoader,
+    //     children: [
+    //       {
+    //         path: "resume",
+    //         element: <Resume />,
+    //         loader: resumeLoader,
+    //       },
+    //     ],
+    //   },
+    // ]);
+
+    // <Router>
+    //   <Routes>
+    //     <Route exact path="/" element={<Home />} />
+    //     <Route path="/resume" element={<Resume />} />
+    //   </Routes>
+    // </Router>
+
+function App() {
+  return (
+    <>
+    <RouterProvider router={router} />
+    </>
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,16 +11,26 @@ import {
   Route,
   Link as RouterLink,
 } from "react-router-dom";
-
-import { Link } from "react-scroll";
+import { NavHashLink } from "react-router-hash-link";
 
 function Header(props) {
+  const scrollWithOffset = (el, offset) => {
+    const elementPosition = el.offsetTop - offset;
+    window.scroll({
+      top: elementPosition,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">
-            <Image src={Logo} alt="" height=""></Image>
+          <Navbar.Brand>
+            <RouterLink to="/">
+              <Image src={Logo} alt="" height="" />
+            </RouterLink>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse
@@ -28,64 +38,58 @@ function Header(props) {
             className="justify-content-end"
           >
             <Nav className="">
-              <Link
-                activeClass="active"
-                to="whatImDoing"
-                spy={true}
-                smooth={true}
-                offset={-25}
-                duration={500}
+              <NavHashLink
+                to="/#whatImDoing"
+                scroll={(el) => scrollWithOffset(el, 45)}
+                className="text-uppercase nav-link text-muted"
               >
-                <Nav.Item className="text-uppercase nav-link">
-                  What I'm doing
-                </Nav.Item>
-              </Link>
+                What Im Doing
+              </NavHashLink>
               <NavDropdown
                 className="text-uppercase"
                 title="Portfolio"
                 id="basic-nav-dropdown"
               >
-                <Link
-                  activeClass="active"
-                  to="portfolio"
-                  spy={true}
-                  smooth={true}
-                  offset={-25}
-                  duration={500}
+                <NavHashLink
+                  to="/#portfolio"
+                  scroll={(el) => scrollWithOffset(el, 45)}
+                  className="text-uppercase nav-link text-muted ps-3"
                 >
-                  <Nav.Item
-                    className="text-uppercase dropdown-item"
-                    href="#action/3.1"
-                  >
-                    All Projects
-                  </Nav.Item>
-                </Link>
-                <NavDropdown.Item className="text-uppercase" href="#action/3.2">
+                  All Projects
+                </NavHashLink>
+                <NavDropdown.Item
+                  className="text-uppercase text-muted"
+                  href="#action/3.2"
+                >
                   Project 2
                 </NavDropdown.Item>
-                <NavDropdown.Item className="text-uppercase" href="#action/3.3">
+                <NavDropdown.Item
+                  className="text-uppercase text-muted"
+                  href="#action/3.3"
+                >
                   Project 3
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item className="text-uppercase" href="#action/3.4">
+                <NavDropdown.Item
+                  className="text-uppercase text-muted"
+                  href="#action/3.4"
+                >
                   Project 4
                 </NavDropdown.Item>
               </NavDropdown>
-              <Link
-                activeClass="active"
-                to="contact"
-                spy={true}
-                smooth={true}
-                offset={-25}
-                duration={500}
+              <NavHashLink
+                to="/#contact"
+                className="text-uppercase nav-link text-muted"
               >
-                <Nav.Item className="text-uppercase nav-link">Contact</Nav.Item>
-              </Link>
-              <Nav.Item className="text-uppercase">
-                <RouterLink to="/resume" className="text-uppercase nav-link">
-                  Resume
-                </RouterLink>
-              </Nav.Item>
+                Contact
+              </NavHashLink>
+              <RouterLink
+                to="/resume"
+                scroll={(el) => scrollWithOffset(el, 45)}
+                className="text-uppercase nav-link"
+              >
+                Resume
+              </RouterLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
