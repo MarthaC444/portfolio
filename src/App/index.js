a5d8ab4a36abdd2b468dde4542b556c8e7b6819f
MarthaC444 <MarthaChamberlain444@gmail.com>
2022-11-09 16:36:40 -0500

    Add FourOhFour component

--- src/App/index.js
+++ src/App/index.js
@@ -1,50 +1,39 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link, createRoutesFromElements, createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import { useRouteError, isRouteErrorResponse } from "react-router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Home from "../views/Home";
 import Resume from "../views/Resume";
+import FourOhFour from "../views/FourOhFour";
 import "./styles.css";
-
-// import Home, { homeLoader } from "../views/Home";
-// import Resume, { resumeLoader } from "../views/Resume";
 
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/" element={<Home />} />
-        <Route path="/resume" element={<Resume />} />
-      </>
-      )
-    );
-    
-    // const router = createBrowserRouter([
-    //   {
-    //     path: "/",
-    //     element: <Home />,
-    //     loader: homeLoader,
-    //     children: [
-    //       {
-    //         path: "resume",
-    //         element: <Resume />,
-    //         loader: resumeLoader,
-    //       },
-    //     ],
-    //   },
-    // ]);
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="*" element={<FourOhFour />} />
+      <Route path="*/*" element={<FourOhFour />} />
 
-    // <Router>
-    //   <Routes>
-    //     <Route exact path="/" element={<Home />} />
-    //     <Route path="/resume" element={<Resume />} />
-    //   </Routes>
-    // </Router>
+    </>
+  )
+);
 
 function App() {
   return (
     <>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </>
   );
 }
