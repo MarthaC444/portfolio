12ab1f925b192fa4ff73109e70333fabb277842a
MarthaC444 <MarthaChamberlain444@gmail.com>
2022-11-13 19:54:20 -0500

    Add social media to header nav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,11 +14,15 @@ function Header(props) {
   return (
     <Navbar expand="lg">
       <Container className="my-4">
+        <Navbar.Brand>
+          <RouterLink to="/">
+            <Image src={Logo} alt="" height="" />
+          </RouterLink>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        {/* <Navbar.Collapse id="basic-navbar-nav" className="justify-content-start"> */}
         <Navbar.Collapse
           id="basic-navbar-nav"
-          className=" d-flex justify-content-between"
+          className="justify-content-between"
         >
           <Nav>
             <HashLink
@@ -46,64 +50,25 @@ function Header(props) {
               Resume
             </RouterLink>
           </Nav>
-          <Row className="d-flex row justify-content-around">
-            <Col className="px-1">
-              <a href="https://github.com/MarthaC444">
-                <i
-                  className="bi bi-github text-muted"
-                  style={{ fontSize: 30 }}
-                ></i>
-              </a>
-            </Col>
-            <Col className="px-1">
-              <a href="https://linkedin.com/in/mchamberlain444">
-                <i
-                  className="bi bi-linkedin text-muted"
-                  style={{ fontSize: 30 }}
-                ></i>
-              </a>
-            </Col>
-          </Row>
         </Navbar.Collapse>
-        <Navbar.Brand>
-          <RouterLink to="/">
-            <Image src={Logo} alt="" height="" />
-          </RouterLink>
-        </Navbar.Brand>
-        {/* <Navbar.Brand>
-          <RouterLink to="/">
-            <Image src={Logo} alt="" height="" />
-          </RouterLink>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav>
-            <HashLink
-              to="/#whatImDoing"
-              className="text-uppercase nav-link text-muted navlink-animation"
-            >
-              What Im Doing
-            </HashLink>
-            <HashLink
-              to="/#portfolio"
-              className="text-uppercase nav-link text-muted navlink-animation"
-            >
-              Portfolio
-            </HashLink>
-            <HashLink
-              to="/#contact"
-              className="text-uppercase nav-link text-muted navlink-animation"
-            >
-              Contact
-            </HashLink>
-            <RouterLink
-              to="/resume"
-              className="text-uppercase nav-link navlink-animation"
-            >
-              Resume
-            </RouterLink>
-          </Nav>
-        </Navbar.Collapse> */}
+        <Row className="d-flex row justify-content-around pe-2">
+          <Col className="px-1">
+            <a href="https://github.com/MarthaC444">
+              <i
+                className="bi bi-github text-muted"
+                style={{ fontSize: 30 }}
+              ></i>
+            </a>
+          </Col>
+          <Col className="px-1">
+            <a href="https://linkedin.com/in/mchamberlain444">
+              <i
+                className="bi bi-linkedin text-muted"
+                style={{ fontSize: 30 }}
+              ></i>
+            </a>
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
