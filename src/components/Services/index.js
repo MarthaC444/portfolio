549dc047367d993ea40d2f2a47d06bbbdf142077
MarthaC444 <MarthaChamberlain444@gmail.com>
2023-07-26 21:23:29 -0400

    Test

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -23,7 +23,7 @@ function Services(props) {
             ></Image>
           </Col>
           <Col sm={12} md={6}>
-            <p className="change-alignment md-text-start">
+            <p className="change-alignment md-text-start ">
               I'm building personal projects with React.js and UI tools like React-Bootstrap and Material UI.
               I'm currently looking for an Apprenticeship or Entry level Jr.
               development role so if you're located in the Tri-Cities area of east
