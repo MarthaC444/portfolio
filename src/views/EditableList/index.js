31d0af59221b747d3f94ecc493f3995ff0608f9b
MarthaC444 <MarthaChamberlain444@gmail.com>
2023-09-19 12:43:01 -0400

    Add working update

--- src/views/EditableList/index.js
+++ src/views/EditableList/index.js
@@ -1,8 +1,14 @@
+// TODO improvements - replace "brute force" replacement of items array with the package deep-equal. Would trigger the render on an actual comparison of state instead of overwriting my whole array.
+// TODO replace math.random with a real id picker
+// TODO create undo last item list?
+// TODO get rid of the originating input button and input and replace with the first editable list item, empty
+// TODO do i need any kind of field validation?
+// TODO react-dom.development.js:86 Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference. Learn more about using refs safely here: https://reactjs.org/link/strict-mode-find-node
 import React, { useState } from "react";
 import Header from "../../components/Header";
 import StickyFooter from "../../components/StickyFooter";
 import Form from "react-bootstrap/Form";
-import { Container, Col, Row, Button } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import "../../App/styles.css";
@@ -12,6 +18,7 @@ function EditableList() {
   const [items, setItems] = useState([]);
 
   function addItem() {
+    // TODO change to if blank just leave blank, no need to remind, just leave it blinking
     if (!newItem) {
       alert("Enter an item");
       return;
@@ -21,6 +28,7 @@ function EditableList() {
       id: Math.floor(Math.random() * 1000),
       value: newItem,
     };
+
     setItems((oldList) => [...oldList, item]);
     setNewItem("");
   }
@@ -28,6 +36,17 @@ function EditableList() {
   function deleteItem(id) {
     const newArray = items.filter((item) => item.id !== id);
     setItems(newArray);
+  }
+  
+  function updateItem(item, newValue) {
+    // TODO is there a better way to pass this id through so I don't have to assign it to variable to do below? Is this screwy?
+    const idNumber= item.id;
+    items[items.findIndex((item => item.id === idNumber))].value = newValue;
+    setItems([...items]);
+  }
+
+  function createInput(item) {
+    return <Form.Control size="lg" type="text" value={item.value} onChange={ e => updateItem(item, e.target.value)} />;
   }
 
   return (
@@ -57,7 +76,8 @@ function EditableList() {
                 className="mb-3"
                 type={"checkbox"}
                 id={`{item.id}`}
-                label={item.value}
+                // label={item.value}
+                label={createInput(item)}
                 onChange={(e) => deleteItem(item.id)}
               />
             </CSSTransition>
