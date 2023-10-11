abeccfc14ebd192b859e04254c7f2805661ed3bc
MarthaC444 <MarthaChamberlain444@gmail.com>
2023-10-11 11:20:08 -0400

    Add App test

--- src/App/__tests__/App.test.js
+++ src/App/__tests__/App.test.js
@@ -1,16 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render  } from '@testing-library/react';
 import App from '../index.js';
 
 describe('Application Tests', () => {
 
-  test('renders App component', () => {
+  it('renders App component', () => {
     render(<App />);
-
-    const jobTitle = screen.getByText(/Jr. Front End Developer \| React.js/u);
-    expect(jobTitle).toBeInTheDocument();
-
-    // const portfolio = screen.getByText(/Portfolio/u);
-    // expect(portfolio).toBeInTheDocument();
   });
 
 });
