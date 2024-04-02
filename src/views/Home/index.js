ba05909c483f151e47cb9c0d957c972f9fb6d34a
MarthaC444 <MarthaChamberlain444@gmail.com>
2024-04-02 14:26:24 -0400

    Add home page load animation

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../components/Header";
 import Hero from "../../components/Hero";
 import Services from "../../components/Services";
@@ -6,17 +6,38 @@ import Portfolio from "../../components/Portfolio";
 import Contact from "../../components/Contact";
 import ScrollToTop from "../../components/ScrollToTop";
 import Footer from "../../components/Footer";
+import Fade from "react-bootstrap/Fade";
 
 function Home() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const playAnimation = () => {
+      setOpen(true);
+    };
+
+    if (document.readyState === "complete") {
+      playAnimation();
+    } else {
+      window.addEventListener("load", playAnimation);
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener("load", playAnimation);
+    }
+  }, []);
+
   return (
     <>
-      <Header />
-      <Hero />
-      <Services />
-      <Portfolio />
-      <Contact />
-      <ScrollToTop />
-      <Footer />
+      <Fade in={open}>
+        <div>
+          <Header />
+          <Hero />
+          <Services />
+          <Portfolio />
+          <Contact />
+          <ScrollToTop />
+          <Footer />
+        </div>
+      </Fade>
     </>
   );
 }
