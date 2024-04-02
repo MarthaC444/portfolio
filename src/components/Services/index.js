d1194b81a83c9a0de2c0e3fc68bec8cb05e1afce
MarthaC444 <MarthaChamberlain444@gmail.com>
2024-04-02 09:34:10 -0400

    Update "doing" copy

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -24,10 +24,19 @@ function Services(props) {
           </Col>
           <Col sm={12} md={6}>
             <p className="change-alignment md-text-start ">
-              I'm building personal projects with React.js and UI tools like React-Bootstrap and Material UI.
+              I'm currently building a wellness and spirituality app that has
+              great features like audio meditations and exercises, self
+              check-ins, reminders, and data visualizations.
+            </p>
+            <p>
+              Technologies I've worked with recently are Typescript, React,
+              Next, Supabase and UI tools like React-Bootstrap and MUI.
+            </p>
+            <p>
               I'm currently looking for an Apprenticeship or Entry level Jr.
-              development role so if you're located in the Tri-Cities area of east
-              Tennessee or have a remote position, I'd love to hear from you!
+              development role so if you're located in the Tri-Cities area of
+              east Tennessee or have a remote position, I'd love to hear from
+              you.
             </p>
           </Col>
         </Row>
