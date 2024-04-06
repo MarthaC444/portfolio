547b1ca5ca9656e7d2111937f1bebea48db1d70c
MarthaC444 <MarthaChamberlain444@gmail.com>
2024-04-06 16:00:03 -0400

    Create and implement updated copyright component

---
+++ src/components/Copyright/index.js
@@ -0,0 +1,9 @@
+function Copyright() {
+  return (
+    <span className="align-self-center text-muted">
+      {"© "} Martha Chamberlain {new Date().getFullYear()}
+    </span>
+  );
+}
+
+export default Copyright;
